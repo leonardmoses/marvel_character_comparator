521f762e69177be458c383e4338df32a9a4ae303
Leonard Moses <qalexeon@Xavier.lan>
2021-09-16 16:37:57 -0400

    changing option on select now change stat values. Some refactoring and cleaning up done

--- js/script.js
+++ js/script.js
@@ -18,6 +18,9 @@ const $eyeColor = $('#eye-color');
 const $hairColor = $('#hair-color');
 let apiData;
 const $leftCharList = $('#leftCharList');
+const leftCharList = document.getElementById('leftCharList');
+let selectText;
+
 // let option;
 // let selectedOption;
 // let select;
@@ -30,64 +33,42 @@ function handleGetNames(event) {
     
     $.ajax({
         url: "https://www.superheroapi.com/api.php/1166343633774129/search/" + leftInput
-
-    }).then(
-        // data parameter
-        (data) => {
-            apiData = data;
-            render();
-            // console.log(apiData);
-            console.log(apiData.results);
-
-            // forEach loop to append to <select>
-            apiData.results.forEach(element => {
-                let option = $('<option>');
-                option.addClass("option");
-                $leftCharList.append(option.text(element.name));
-            });
-
-        },
-        // error parameter
-        (error) => {
-            console.log("Character not found", error);
-            
-        }
-    );
- 
-    
-
-    
+    }).then((data) => {
+                apiData = data;
+                render();
+                console.log(apiData.results);
+                // forEach loop to append to <select>
+                apiData.results.forEach(element => {
+                    let option = $('<option>');
+                    option.addClass("option");
+                    $leftCharList.append(option.text(element.name));
+                });
+            },(error) => {
+                console.log("Character not found", error);
+            }
+        );
 }
 
-/*
-function handleGetData(event) {
-    event.preventDefault();
+function handleSelect(event) {
+    select = event.target;
+    selectText = select.value;
+    console.log(selectText);
 
-    select = document.getElementById("select");
-    select.onchange = function(){
-        selectedOption = this.value; //returns the selected value
-        alert(this.innerHTML); //returns the entire select with all the options
-        let options = this.getElementsByTagName("option");
-        let optionHTML = options[this.selectedIndex].innerHTML;  
-        alert(optionHTML); //this is what I want, but it works now
-    
-    }
 
     $.ajax({
-        url: "https://www.superheroapi.com/api.php/1166343633774129/search/" + leftInput
+        url: "https://www.superheroapi.com/api.php/1166343633774129/search/" + selectText
+    }).then((data) => {
+                apiData = data;
+                render();
+                console.log(apiData);
+                // console.log(apiData.results);
 
-    }).then(
-        (data) => {
-            render();
-            console.log(apiData.results);
-        },
-        (error) => {console.log("Could not get Chacter or Stats");}
-
-    );
-
-    console.log(selectedOption);
+            },(error) => {
+                console.log("Character not found", error);
+            }
+        );
 }
-*/
+
 
 
 // render function. Renders/returns apiData.results 
@@ -95,26 +76,13 @@ function render() {
     $charName1.text(apiData.results[0].name);
     $intelligence.text(apiData.results[0].powerstats.intelligence);
     $strength.text(apiData.results[0].powerstats.strength);
-    // $speed.text(apiData.results[0].powerstats.speed);
-    // $durability.text(apiData.results[0].powerstats.durability);
-    // $power.text(apiData.results[0].powerstats.power)
+    $speed.text(apiData.results[0].powerstats.speed);
+    $durability.text(apiData.results[0].powerstats.durability);
+    $power.text(apiData.results[0].powerstats.power)
 }
 
 
 
 $('#leftForm').on('submit' , handleGetNames);
 
-
-let A = document.getElementById('leftCharList');
-
-A.addEventListener('input', handleSelect);
-
-function handleSelect(event) {
-    select = event.target;
-    console.log(select.value);
-}
-
-function handleText(event) {
-    theInput = event.target;
-}
-
+leftCharList.addEventListener('input', handleSelect);
